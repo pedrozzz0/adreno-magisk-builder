465074b4f0073ed2ea660c812ddf4ee1c7028dff
pedrozzz0 <guitopzika26@gmail.com>
2023-06-01 23:46:06 -0300

    Fixup issues
    ** Fixup all major issues, properly fetch /vendor/firmware dependencies and other improvements
    
    Signed-off-by: pedrozzz0 <guitopzika26@gmail.com>

--- adreno_builder.sh
+++ adreno_builder.sh
@@ -2,6 +2,9 @@
 # Adreno Magisk Driver Builder
 # GPL 3.0 or later
 # By p3dr0 (pedrozzz0 @ Github)
+# TODO: Dynamically download *_sqe and *_gmu
+
+set +x
 
 if [ $# -eq 0 ]; then
     echo "Adreno GPU Driver Builder 1.0
@@ -25,6 +28,9 @@ url4="https://dumps.tadiphone.dev/dumps/$1/$2/-/raw/$3/vendor/lib64/egl/"
 url5="https://dumps.tadiphone.dev/dumps/$1/$2/-/raw/$3/vendor/lib/hw/"
 url6="https://dumps.tadiphone.dev/dumps/$1/$2/-/raw/$3/vendor/lib64/hw/"
 url7="https://dumps.tadiphone.dev/dumps/$1/$2/-/raw/$3/vendor/gpu/kbc/"
+url8="https://dumps.tadiphone.dev/dumps/$1/$2/-/raw/$3/vendor/firmware/"
+url9="https://dumps.tadiphone.dev/dumps/$1/$2/-/raw/$3/system/system/lib/"
+url10="https://dumps.tadiphone.dev/dumps/$1/$2/-/raw/$3/system/system/lib64/"
 
 [ ! -d $(pwd)/system ] && {
 mkdir -p $(pwd)/system/vendor/firmware \
@@ -32,7 +38,9 @@ mkdir -p $(pwd)/system/vendor/firmware \
          $(pwd)/system/vendor/lib/egl \
          $(pwd)/system/vendor/lib/hw \
 		 $(pwd)/system/vendor/lib64/egl \
-		 $(pwd)/system/vendor/lib64/hw
+		 $(pwd)/system/vendor/lib64/hw \
+		 $(pwd)/system/system/lib \
+		 $(pwd)/system/system/lib64
 }
 
 move_file() {
@@ -42,7 +50,10 @@ move_file() {
   shift 2 # Shift to skip URL and destination
 
   for lib do
-    curl -OJL "${url}${lib}"
+    if ! curl -Sf -OJL "${url}${lib}"; then
+      echo "Error: Failed to download ${lib} from ${url}${lib}"
+      continue
+    fi
     mv "$lib" "$destination"
   done
 }
@@ -52,7 +63,6 @@ move_file "$url1" "$(pwd)/system/vendor/lib/" \
   libadreno_utils.so \
   libbase64.so \
   libCB.so \
-  libdmabufheap.so \
   libkcl.so \
   libEGL_adreno.so \
   libkernelmanager.so \
@@ -75,7 +85,6 @@ move_file "$url2" "$(pwd)/system/vendor/lib64/" \
   libadreno_utils.so \
   libbase64.so \
   libCB.so \
-  libdmabufheap.so \
   libkcl.so \
   libEGL_adreno.so \
   libkernelmanager.so \
@@ -126,7 +135,31 @@ move_file "$url7" "$(pwd)/system/vendor/gpu/kbc/" \
   unified_kbcs_64.bin \
   unified_ksqs.bin 
 
-[ ! -d $(pwd)/META-INF ] && {
+move_file "$url8" "$(pwd)/system/vendor/firmware/" \
+  a619_sqe.fw \
+  a619_gmu.bin \
+  a630_sqe.fw \
+  a630_gmu.bin \
+  a650_sqe.fw \
+  a650_gmu.bin \
+  a660_sqe.fw \
+  a660_gmu.bin \
+  a662_sqe.fw \
+  a662_gmu.bin \
+  a710_sqe.fw \
+  a710_gmu.bin \
+  a730_sqe.fw \
+  a730_gmu.bin \
+  a740_sqe.fw \
+  a740_gmu.bin
+
+move_file "$url9" "$(pwd)/system/system/lib/" \
+  libdmabufheap.so
+
+move_file "$url10" "$(pwd)/system/system/lib64/" \
+  libdmabufheap.so
+
+if [ ! -d "$(pwd)/META-INF" ]; then
 mkdir -p $(pwd)/META-INF/com/google/android/
 echo "#!/sbin/sh
 
@@ -137,36 +170,37 @@ echo "#!/sbin/sh
 umask 022
 
 # echo before loading util_functions
-ui_print() { echo "$1"; }
+ui_print() { echo \"\$1\"; }
 
 require_new_magisk() {
-	ui_print "*******************************"
-	ui_print " Please install Magisk v20.4+! "
-	ui_print "*******************************"
-	exit 1
+    ui_print \"*******************************\"
+    ui_print \" Please install Magisk v20.4+! \"
+    ui_print \"*******************************\"
+    exit 1
 }
 
 #########################
 # Load util_functions.sh
 #########################
 
-OUTFD=$2
-ZIPFILE=$3
+OUTFD=\$2
+ZIPFILE=\$3
 
 mount /data 2>/dev/null
 
-[[ -f "/data/adb/magisk/util_functions.sh" ]] || require_new_magisk
+[[ -f \"/data/adb/magisk/util_functions.sh\" ]] || require_new_magisk
 . /data/adb/magisk/util_functions.sh
 
-[[ "$MAGISK_VER_CODE" -lt "20400" ]] && require_new_magisk
+[[ \"\$MAGISK_VER_CODE\" -lt \"20400\" ]] && require_new_magisk
 
 install_module
 exit 0
 " > $(pwd)/META-INF/com/google/android/update-binary
 echo "#MAGISK" > $(pwd)/META-INF/com/google/android/updater-script
-}
+fi
 
-[ ! $(pwd)/customize.sh ] && echo "MODVER=$(grep_prop version $MODPATH/module.prop)
+if [ ! -f $(pwd)/customize.sh ]; then
+echo 'MODVER=$(grep_prop version $MODPATH/module.prop)
 MODVERCODE=$(grep_prop versionCode $MODPATH/module.prop)
 ui_print "- Adreno GPU Driver by p3dr0"
 ui_print ""
@@ -206,7 +240,7 @@ set_perm_recursive $MODPATH/system/vendor/lib/libadreno_app_profiles.so 0 0 0755
 set_perm_recursive $MODPATH/system/vendor/lib/libadreno_utils.so 0 0 0755 0644 u:object_r:same_process_hal_file:s0
 set_perm_recursive $MODPATH/system/vendor/lib/libbase64.so 0 0 0755 0644 u:object_r:same_process_hal_file:s0
 set_perm_recursive $MODPATH/system/vendor/lib/libCB.so 0 0 0755 0644 u:object_r:same_process_hal_file:s0
-set_perm_recursive $MODPATH/system/vendor/lib/libdmabufheap.so 0 0 0755 0644 u:object_r:same_process_hal_file:s0
+set_perm_recursive $MODPATH/system/system/lib/libdmabufheap.so 0 0 0755 0644 u:object_r:same_process_hal_file:s0
 set_perm_recursive $MODPATH/system/vendor/lib/libEGL_adreno.so 0 0 0755 0644 u:object_r:same_process_hal_file:s0
 set_perm_recursive $MODPATH/system/vendor/lib/libgame_enhance.so 0 0 0755 0644 u:object_r:same_process_hal_file:s0
 set_perm_recursive $MODPATH/system/vendor/lib/libGLESv2_adreno.so 0 0 0755 0644 u:object_r:same_process_hal_file:s0
@@ -228,7 +262,7 @@ set_perm_recursive $MODPATH/system/vendor/lib64/libadreno_app_profiles.so 0 0 07
 set_perm_recursive $MODPATH/system/vendor/lib64/libadreno_utils.so 0 0 0755 0644 u:object_r:same_process_hal_file:s0
 set_perm_recursive $MODPATH/system/vendor/lib64/libbase64.so 0 0 0755 0644 u:object_r:same_process_hal_file:s0
 set_perm_recursive $MODPATH/system/vendor/lib64/libCB.so 0 0 0755 0644 u:object_r:same_process_hal_file:s0
-set_perm_recursive $MODPATH/system/vendor/lib64/libdmabufheap.so 0 0 0755 0644 u:object_r:same_process_hal_file:s0
+set_perm_recursive $MODPATH/system/system/lib64/libdmabufheap.so 0 0 0755 0644 u:object_r:same_process_hal_file:s0
 set_perm_recursive $MODPATH/system/vendor/lib64/libEGL_adreno.so 0 0 0755 0644 u:object_r:same_process_hal_file:s0
 set_perm_recursive $MODPATH/system/vendor/lib64/libgame_enhance.so 0 0 0755 0644 u:object_r:same_process_hal_file:s0
 set_perm_recursive $MODPATH/system/vendor/lib64/libGLESv2_adreno.so 0 0 0755 0644 u:object_r:same_process_hal_file:s0
@@ -246,8 +280,24 @@ set_perm_recursive $MODPATH/system/vendor/lib64/libopencv3a.so 0 0 0755 0644 u:o
 set_perm_recursive $MODPATH/system/vendor/lib64/libopenvx.so 0 0 0755 0644 u:object_r:same_process_hal_file:s0
 set_perm_recursive $MODPATH/system/vendor/lib64/libq3dtools_adreno.so 0 0 0755 0644 u:object_r:same_process_hal_file:s0
 set_perm_recursive $MODPATH/system/vendor/lib64/libVkLayer_q3dtools.so 0 0 0755 0644 u:object_r:same_process_hal_file:s0
+chmod 644 /system/vendor/firmware/a619_sqe.fw
+chmod 644 /system/vendor/firmware/a619_gmu.bin
+chmod 644 /system/vendor/firmware/a630_sqe.fw
+chmod 644 /system/vendor/firmware/a630_gmu.bin
+chmod 644 /system/vendor/firmware/a650_sqe.fw
+chmod 644 /system/vendor/firmware/a650_gmu.bin
+chmod 644 /system/vendor/firmware/a660_sqe.fw
+chmod 644 /system/vendor/firmware/a660_gmu.bin
+chmod 644 /system/vendor/firmware/a662_sqe.fw
+chmod 644 /system/vendor/firmware/a662_gmu.bin
+chmod 644 /system/vendor/firmware/a710_sqe.fw
+chmod 644 /system/vendor/firmware/a710_gmu.bin
+chmod 644 /system/vendor/firmware/a730_sqe.fw
+chmod 644 /system/vendor/firmware/a730_gmu.bin
+chmod 644 /system/vendor/firmware/a740_sqe.fw
+chmod 644 /system/vendor/firmware/a740_gmu.bin
 
-# Cache Cleaner
+# Shader cache
 CACHE=$(find /data/user_de -name *shaders_cache* -type f | grep code_cache)
 for i in $CACHE; do
     rm -rf $i
@@ -260,30 +310,35 @@ done
 ui_print ""
 ui_print "- Reboot to the drivers be applied"
 ui_print ""
-" > $(pwd)/customize.sh
+' > $(pwd)/customize.sh
 
-[ ! $(pwd)/system.prop ] && echo "# Zygote HideCrash Fix
+if [ ! -f "$(pwd)/system.prop" ]; then
+echo "# Zygote HideCrash Fix
 ro.zygote.disable_gl_preload=1
 " > $(pwd)/system.prop
+fi
 
-[ ! $(pwd)/module.prop ] && echo "id=adreno_driver
+if [ ! -f "$(pwd)/module.prop" ]; then
+echo "id=adreno_driver
 name=Adreno GPU driver
 version=v1
 versionCode=1
 author=pedro
 description=OpenGL & Vulkan driver
 " > $(pwd)/module.prop
+fi
 
 if [ $(command -v zip) ]; then
 	echo "Process done, zip the driver?"
-	read zip
-	if [ "$zip" = "Y" ] || [ "$zip" = "y" ]; then
+	read compact
+	if [ "$compact" = "Y" ] || [ "$compact" = "y" ]; then
 	echo "Input the driver name: "
 	read driver_name
+	fi
 	if [ "$driver_name" = "" ]; then
 	driver_name="Adreno_GPU_Driver"
 	fi
-	zip -r9 -ll "$driver_name.zip" . -x adreno_builder.sh
+	zip -r9 "$driver_name.zip" . -x adreno_builder.sh -x README.md -x LICENSE -x *.git*
 	fi
 	echo "Done!"
 else
